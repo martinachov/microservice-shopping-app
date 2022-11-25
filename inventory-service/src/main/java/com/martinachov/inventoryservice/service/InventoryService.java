4c3b89c827cff4cfc5dab71ac894a5b6d87d17e4
martinachov <martin1082v@gmail.com>
2022-11-25 14:14:29 -0300

    Circuit Breaker

--- inventory-service/src/main/java/com/martinachov/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/martinachov/inventoryservice/service/InventoryService.java
@@ -12,15 +12,24 @@ import com.martinachov.inventoryservice.model.Inventory;
 import com.martinachov.inventoryservice.repository.InventoryRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCodes) {
+        
+        log.info("Wait Started");
+        // Thread.sleep(10000);
+        log.info("Wait Ended");
+
         List<Inventory> listInventories = inventoryRepository.findBySkuCodeIn(skuCodes);
         return listInventories.stream()
                                 .map(inventory -> InventoryResponse.builder()
